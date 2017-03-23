a550ecafa991bf9ee13e58d920c4611d8a352759
rpwr021 <rpwr021@users.noreply.github.com>
2017-03-22 19:59:28 -0500

    Create json_to_DF.R
    json.R

---
+++ json_to_DF.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(jsonlite)
+library(ggmap)
+library(knitr)
+
+################################################################################
+##        READ DATA
+################################################################################
+
+training <- fromJSON("/home/raz/Documents/renthop/train.json") %>% 
+  bind_rows 
+features <- training$features
+photos <- training$photos
+
+training$features <- NULL
+training$photos <- NULL 
+
+# Convert to data.frame
+training <- sapply(training, unlist) %>%
+  data.frame(., stringsAsFactors = FALSE)
+
+# Add removed variables
+training$features <- features
+training$photos <- photos
+
+training$numFeat <- NULL
+training$numFeat <- NULL 
+
+for(i in 1:length(training$photos))
+{
+  training$numPh[i] <- length(training$photos[[i]])
+  training$numFeat[i] <- length(training$features[[i]])
+}
+
+numerical_variables <- c("bathrooms", "bedrooms",
+                         "longitude", "latitude", "price")
+
+training[, numerical_variables] %<>%
+  lapply(., as.numeric)
+
+training$interest_level <- as.factor(training$interest_level)
+
