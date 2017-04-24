28054247a518eb0e299cf6aea071dab75a4af503
Shuyu Zhang <szhang59@hawk.iit.edu>
2017-04-23 20:22:46 -0500

    Updated baseline_EDA.R and randomforest.R

--- Models/baseline_model/baseline_EDA.R
+++ Models/baseline_model/baseline_EDA.R
@@ -2,12 +2,21 @@ library(nnet)
 library(ggplot2)
 library(lattice)
 
-train_base <- read.csv('../../processed_data/train_baseline11_v2.csv',header = TRUE)
+# train_base <- read.csv('../processed_data/train_baseline11_v2.csv',header = TRUE)
+# train_base_add <- read.csv('../processed_data/train_baselineNEW.csv',header = TRUE)
+train_base <- read.csv('../../processed_data/train_baselineCLEAN.csv',header = TRUE)
+# train_base <- merge(train_base, train_base_add)
 str(train_base)
 
 # reorder interest levels
 train_base$interest_level <- factor(train_base$interest_level, 
                                     levels = c("low", "medium", "high"))
+
+# plot the distribution of interest level
+ggplot(aes (x = interest_level), data = train_base) + 
+  geom_bar(color = 'black', fill = "darkgreen", alpha = 0.5)
+# ggsave("../Plots/Interest Level Distribution.png",width = 21, height = 20, units = 'cm')
+
 # univariate 
 # bedrooms
 summary(train_base$bedrooms)
@@ -15,68 +24,133 @@ with(train_base, table(interest_level, bedrooms))
 
 by(train_base$bedrooms, train_base$interest_level, summary)
 ggplot(aes(x = bedrooms), data = train_base, binwidth = 1) + 
-  geom_histogram(color = 'black', aes(fill=..count..), 
-                 breaks=seq(0, 8, by = 1), alpha = .2) +
-  labs(title="Histogram for Bedrooms") +
-  labs(x="Bedrooms", y="Count") +  scale_fill_gradient("Count", low = "green", high = "red") + 
-  facet_wrap( ~ interest_level)
+  geom_bar(color = 'black', aes(fill= interest_level), alpha = .5) +
+  labs(title="Bar for Bedrooms") + scale_x_continuous(breaks=seq(0, 8, 1))
+# ggsave("../Plots/Barplot of Bedrooms.png", width = 21, height = 20, units = 'cm')
 
 # bathrooms
+# outlier detection
+train_base[which(train_base$bathrooms>2*train_base$bedrooms & train_base$bedrooms!=0), 2:3]
+
+#        bedrooms bathrooms
+# 2076         1       3.0
+# 3088         1       3.0
+# 4422         2      10.0
+# 5385         1       3.0
+# 13960        1       3.0
+# 15422        1       4.5
+# 24365        1       3.0
+# 24695        1       3.0
+# 24737        1       3.0
+# 28838        1       3.0
+# 33076        1       3.0
+# 39257        1       3.0
+# 41658        1       3.0
+# 41744        1       2.5
+# 45907        1       3.0
+# 46689        1       2.5
+# 47733        1       3.0
+# 47935        1       3.0
+
 by(train_base$bathrooms, train_base$interest_level, summary)
 with(train_base, table(interest_level, bathrooms))
 
 summary(train_base$bathrooms)
-ggplot(aes(x = bathrooms), data = train_base, binwidth = 1) + 
-  geom_histogram(color = 'black', aes(fill=..count..), 
-                 breaks=seq(0, 8, by = 1), alpha = .2) +
-  labs(title="Histogram for Bathrooms") +
-  labs(x="Bathrooms", y="Count") +  scale_fill_gradient("Count", low = "green", high = "red") + 
-  facet_wrap( ~ interest_level)
+
+ggplot(aes(x = bathrooms), data = train_base) + 
+  geom_histogram(color = 'black', aes(fill= interest_level), alpha = .5 )+
+  labs(title="Barplot for Bathrooms") + coord_cartesian(xlim = c(0,6)) +
+  scale_x_continuous(breaks=seq(0, 6, 0.5))
+# ggsave("Barplot of Bathrooms.png", width = 21, height = 20, units = 'cm')
 
 # price
 by(train_base$price, train_base$interest_level, summary)
 summary(train_base$price)
 ggplot(train_base, aes(x=(price))) + 
-  geom_histogram(color = 'black', aes(fill=..count..), alpha = .2) + 
-  xlim(c(1000,11000)) + facet_wrap( ~ interest_level)
+  geom_histogram(color = 'black', aes(fill=interest_level), alpha = .5) + 
+  xlim(c(1000,11000)) + labs(title = "Histogram of Bathrooms")
+# ggsave("Histogram of Bathrooms.png", width = 21, height = 20, units = 'cm')
+
 
 # number of photos
 by(train_base$numPh, train_base$interest_level, summary)
 summary(train_base$numPh)
 ggplot(train_base, aes(x=(numPh)), binwidth = 1) + 
-  geom_histogram(color = 'black', aes(fill=..count..), alpha = .2) + xlim(c(0, 30)) + 
-  facet_wrap( ~ interest_level)
+  geom_histogram(color = 'black', aes(fill=interest_level), alpha = .5) + xlim(c(0, 30)) +
+  labs(title = "Histogram of Number of Photos")
+# ggsave("Histogram of NumPhoto.png", width = 21, height = 20, units = 'cm')
 
 # number of features
 by(train_base$numFeat, train_base$interest_level, summary)
 summary(train_base$numFeat)
 ggplot(train_base, aes(x=(numFeat))) + 
-  geom_histogram(color = 'black', aes(fill=..count..), alpha = .2) + xlim(c(0,30)) +
-  facet_wrap( ~ interest_level)
+  geom_histogram(color = 'black', aes(fill=interest_level), alpha = .5) + xlim(c(0,30)) +
+  labs(title = "Histogram of Number of Features")
+
+# ggsave("Histogram of NumFeatures.png", width = 21, height = 20, units = 'cm')
 
 
 # distance to city
 by(train_base$distance_city, train_base$interest_level, summary)
+
 summary(train_base$distance_city)
-ggplot(train_base, aes(x=(distance_city))) + 
-  geom_histogram(color = 'black', aes(fill=..count..), alpha = .2) + xlim(c(0,1)) + 
-  facet_wrap( ~ interest_level)
+ggplot(train_base, aes(x=(distance_city), colour = interest_level) ) + 
+  geom_density() + xlim(c(0,0.3)) +
+  labs(title = "Density Plot of Distance to City")
+# ggsave("Density Plot of Distance to City.png", width = 21, height = 20, units = 'cm')
+
 
 # created month
 by(train_base$created_month, train_base$interest_level, summary)
-qplot(created_month, data = train_base) + facet_wrap( ~ interest_level)
+qplot(created_month, data = train_base) + facet_wrap( ~ interest_level, scales = "free_y")
 
 # created at weekend
 by(train_base$weekend, train_base$interest_level, summary)
-qplot(weekend, data = train_base) + facet_wrap( ~ interest_level)
+qplot(weekend, data = train_base) + facet_wrap( ~ interest_level, scales = "free_y")
 
-# There is no effective evidence showing that interest_level is related to the created_date
+# created hour
+ggplot(aes(x = created_hour), data = train_base) + geom_bar(aes(fill = interest_level)) +
+  scale_x_continuous(breaks=seq(0, 23, 1)) + labs(title = "Barplot of Created Hour")
+ggsave("/Barplot of Created Hour.png", width = 40, height = 20, units = 'cm')
 
+# time of day created
+ggplot(aes(x = time_ofday), data = train_base) + geom_bar(aes(fill = interest_level)) +
+  labs(title = "Barplot of Time of Day")
+ggsave("Barplot of Time of Day.png", width = 40, height = 20, units = 'cm')
 
+# Result shows that created_hour might effect the interest
+
+# price and number of bedrooms
+library(dplyr)
+bedroom_group <- group_by(train_base, bedrooms)
+
+train_base.price_by_bd <- train_base %>%
+  group_by(bedrooms) %>%
+  summarise(price_mean = mean(price),
+            price_median = median(price), 
+                            #number of users
+            n = n())  %>%
+  arrange(bedrooms)
+head(train_base.price_by_bd)
+
+# price and number of bathroom
+bathroom_group <- group_by(train_base, bathrooms)
+
+train_base.price_by_bath <- train_base %>%
+  group_by(bathrooms) %>%
+  summarise(price_mean = mean(price),
+            price_median = median(price), 
+            #number of users
+            n = n())  %>%
+  arrange(bathrooms)
+head(train_base.price_by_bath)
+
+library(corrplot)
 # print the coefficients
 cor(train_base$interest_level_num, train_base$bedrooms)
 cor(train_base$interest_level_num, train_base$bathrooms)
 cor(train_base$interest_level_num, train_base$weekend)
+
 table(train_base$interest_level_num, as.factor(train_base$weekend))
 
 #   FALSE  TRUE
@@ -91,9 +165,6 @@ table(train_base$interest_level_num, as.factor(train_base$weekend))
 
 # 
 # histogram(~bathrooms+bedrooms | interest_level, train_base)
-
-# build baseline model
-t <- multinom(interest_level ~ bedrooms + bathrooms + price + weekend + created_month, data = train_base)
 
 # ------finished EDA----------
 
--- Models/randomForest.R
+++ Models/randomForest.R
@@ -7,7 +7,7 @@ library(caret)
 ########################
 # train_base <- read.csv('../processed_data/train_baseline11_v2.csv', header = TRUE)
 # train_base1  <- read.csv('../processed_data/train_baselineNEW.csv', header = TRUE)
-train <- read.csv('../../processed_data/train_baseline11_v3.csv', header = TRUE)
+train <- read.csv('../../processed_data/train_baselineCLEAN.csv', header = TRUE)
 # train_des_st = read.csv('../../processed_data/train_description_sentiment.csv', header = TRUE)
 # train_des_tfidf = read.csv('../../processed_data/train_description_wordcount_tfidf.csv', header = TRUE)
 
@@ -25,13 +25,13 @@ sapply(train, class)
 # sapply(train_des_tfidf, class)
 # sapply(train_des_st, class)
 
-numVars = c('bedrooms', 'bathrooms', 'price','numFeat', 'numPh', 'distance_city')
+numVars = c('bedrooms', 'bathrooms', 'price','numFeat', 'numPh', 'distance_city', 'clean_wordcount')
 # ,
 #             names(train_des_st)[!names(train_des_st) %in% c("listing_id", "interest_level", "interest_Nbr")],
 #             names(train_des_tfidf)[!names(train_des_tfidf) %in% c("listing_id", "interest_level", "interest_Nbr")]
 # )
 
-catVars = c('weekend', 'created_month', 'created_hour')
+catVars = c('weekend', 'created_month', 'created_hour','time_ofday')
 for(var in catVars){
   train[var] = lapply(train[var], factor)
 }
@@ -56,13 +56,13 @@ createModelFormula <- function(targetVar, xVars, includeIntercept = TRUE){
 # Split into train and test sets
 
 ## adding time_ofday to the model 
-
-time.tag <-  c("00", "06", "12", "18", "24")
-
-train$time_ofday <- cut(as.numeric(train$created_hour), breaks=time.tag,
-                        labels = c("00-06","06-12", "12-18", "18-23"), include.lowest= TRUE)
-
-train$time_ofday <- as.factor(train$time_ofday)
+# 
+# time.tag <-  c("00", "06", "12", "18", "24")
+# 
+# train$time_ofday <- cut(as.numeric(train$created_hour), breaks=time.tag,
+#                         labels = c("00-06","06-12", "12-18", "18-23"), include.lowest= TRUE)
+# 
+# train$time_ofday <- as.factor(train$time_ofday)
 
 ##
 inTrain <- createDataPartition(y = train$interest_level_num, list = FALSE, p = 0.8)
@@ -91,13 +91,44 @@ colnames(compare) = c('predicted', 'real')
 conf_matrix <- table(compare$predicted, compare$real)
 confusionMatrix(conf_matrix)
 
-##
+############### Result with clean_wordcount (CLEAN.csv)  ###################
+# Confusion Matrix and Statistics
+# 
+# 
+# 1    2    3
+# 1  171   52  101
+# 2  321 6385 1494
+# 3  275  449  621
+# 
+# Overall Statistics
+# 
+# Accuracy : 0.7272         
+# 95% CI : (0.7183, 0.736)
+# No Information Rate : 0.6977         
+# P-Value [Acc > NIR] : 6.451e-11      
+# 
+# Kappa : 0.2954         
+# Mcnemar's Test P-Value : < 2.2e-16      
+# 
+# Statistics by Class:
+# 
+# Class: 1 Class: 2 Class: 3
+# Sensitivity           0.22295   0.9272  0.28023
+# Specificity           0.98319   0.3916  0.90540
+# Pos Pred Value        0.52778   0.7787  0.46171
+# Neg Pred Value        0.93756   0.6998  0.81288
+# Prevalence            0.07772   0.6977  0.22454
+# Detection Rate        0.01733   0.6470  0.06292
+# Detection Prevalence  0.03283   0.8309  0.13629
+# Balanced Accuracy     0.60307   0.6594  0.59282
+
+############### Result with clean_wordcount ###################
 
 require(doMC)
 require(caret)
 doMC::registerDoMC(cores=3)
 
-modelForm1 <- createModelFormula(targetVar, c(catVars, numVars, "time_ofday"))
+modelForm1 <- createModelFormula(targetVar, c(catVars, numVars))
 
 control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
 fit1 <-  randomForest(modelForm1, data = train.train, importance = TRUE, 
@@ -109,43 +140,41 @@ colnames(compare) = c('predicted', 'real')
 conf_matrix <- table(compare$predicted, compare$real)
 confusionMatrix(conf_matrix)
 
-
-# 
+############### Tuning after adding clean_wordcount  (CLEAN.csv) #############
 # Confusion Matrix and Statistics
 # 
 # 
 # 1    2    3
-# 1  145   30   86
-# 2  423 6526 1752
-# 3  199  264  441
+# 1  176   53  113
+# 2  319 6385 1472
+# 3  272  448  631
 # 
 # Overall Statistics
 # 
-# Accuracy : 0.7209          
-# 95% CI : (0.7119, 0.7297)
-# No Information Rate : 0.6913          
-# P-Value [Acc > NIR] : 7.239e-11       
+# Accuracy : 0.7287          
+# 95% CI : (0.7199, 0.7375)
+# No Information Rate : 0.6977          
+# P-Value [Acc > NIR] : 6.715e-12       
 # 
-# Kappa : 0.2397          
+# Kappa : 0.3018          
 # Mcnemar's Test P-Value : < 2.2e-16       
 # 
 # Statistics by Class:
 # 
 # Class: 1 Class: 2 Class: 3
-# Sensitivity           0.18905   0.9569  0.19351
-# Specificity           0.98725   0.2859  0.93897
-# Pos Pred Value        0.55556   0.7500  0.48783
-# Neg Pred Value        0.93524   0.7476  0.79491
-# Prevalence            0.07774   0.6913  0.23100
-# Detection Rate        0.01470   0.6615  0.04470
-# Detection Prevalence  0.02645   0.8819  0.09163
-# Balanced Accuracy     0.58815   0.6214  0.56624
-# ##
+# Sensitivity           0.22947   0.9272  0.28475
+# Specificity           0.98176   0.3996  0.90592
+# Pos Pred Value        0.51462   0.7809  0.46706
+# Neg Pred Value        0.93797   0.7041  0.81392
+# Prevalence            0.07772   0.6977  0.22454
+# Detection Rate        0.01783   0.6470  0.06394
+# Detection Prevalence  0.03465   0.8285  0.13689
+# Balanced Accuracy     0.60561   0.6634  0.59533
+################### Tuning after adding clean_wordcount  ############
 
 
 
-
-################ without sentiment #######################
+################ without sentiment  (11_v3.csv) #######################
 # Confusion Matrix and Statistics
 # 
 # 
@@ -177,11 +206,41 @@ confusionMatrix(conf_matrix)
 # Balanced Accuracy     0.61343   0.6624  0.59275
 ################ without sentiment #######################
 
+#### Tuning with (11_v3.csv)    ################
+# Confusion Matrix and Statistics
+# 
+# 
+# 1    2    3
+# 1  145   30   86
+# 2  423 6526 1752
+# 3  199  264  441
+# 
+# Overall Statistics
+# 
+# Accuracy : 0.7209          
+# 95% CI : (0.7119, 0.7297)
+# No Information Rate : 0.6913          
+# P-Value [Acc > NIR] : 7.239e-11       
+# 
+# Kappa : 0.2397          
+# Mcnemar's Test P-Value : < 2.2e-16       
+# 
+# Statistics by Class:
+# 
+# Class: 1 Class: 2 Class: 3
+# Sensitivity           0.18905   0.9569  0.19351
+# Specificity           0.98725   0.2859  0.93897
+# Pos Pred Value        0.55556   0.7500  0.48783
+# Neg Pred Value        0.93524   0.7476  0.79491
+# Prevalence            0.07774   0.6913  0.23100
+# Detection Rate        0.01470   0.6615  0.04470
+# Detection Prevalence  0.02645   0.8819  0.09163
+# Balanced Accuracy     0.58815   0.6214  0.56624
+#### Tuning with (11_v3.csv)    ################
 
 
 
-
-##################  with sentiment ############################
+##################  with sentiment (11_v3.csv) ############################
 
 # Confusion Matrix and Statistics
 # 
@@ -213,44 +272,5 @@ confusionMatrix(conf_matrix)
 # Detection Prevalence  0.02686   0.8839  0.08920
 # Balanced Accuracy     0.59288   0.6178  0.56455
 ##################  with sentiment ############################
-
-
-########################
-# cross validation for accuracy
-########################
-
-cross_validation = function(data = data, targetVar = targetVar, xVars = xVars, includeIntercept = TRUE){
-  accuracy = numeric()
-  for(i in 1:10){
-    if(i<10){
-      fold = paste('Fold0', i, sep = '')
-    }else{
-      fold = paste('Fold', i, sep = '')
-    }
-    testIndexes <- createFolds(data$interest_level, k=10, list=TRUE)[[fold]]
-    testData <- data[testIndexes, ]
-    trainData <- data[-testIndexes, ]
-    
-    modelForm = createModelFormula(targetVar, xVars, includeIntercept)
-    model <- randomForest(modelForm, data = trainData)
-    
-    predicted = predict(model, testData)
-    compare = as.data.frame(cbind(predicted, testData$interest_level))
-    colnames(compare) = c('predicted', 'real')
-    accuracy = c(accuracy, mean(compare$predicted == compare$real))
-    print(accuracy)
-  }
-  return(accuracy)
-}
-
-accuracy = cross_validation(train, targetVar, c(catVars, numVars))
-accuracy
-
-# [1] 0.7947315 0.7811994 0.7932712 0.7864235 0.7829787 0.7847588
-# [7] 0.7857722 0.7929078 0.7824149 0.7921394
-
-mean(accuracy)
-
-
 
 
