6847e41438d5dbe78dd9a1e7b16cde611654485d
rpwr021 <rpawar.021@gmail.com>
2017-04-25 12:13:52 -0500

    done

---
+++ Models/RF_forKaggle.R
@@ -0,0 +1,68 @@
+library(nnet)
+library(caret)
+
+########################
+# load all processed data
+########################
+setwd("/home/raz/Projects/TEMP/EDA_features/Models")
+# train_base <- read.csv('../processed_data/train_baseline11_v2.csv', header = TRUE)
+# train_base1  <- read.csv('../processed_data/train_baselineNEW.csv', header = TRUE)
+train <- read.csv('../processed_data/train_moreFeat36.csv', header = TRUE)
+
+########################
+createModelFormula <- function(targetVar, xVars, includeIntercept = TRUE){
+  if(includeIntercept){
+    modelForm <- as.formula(paste(targetVar, "~", paste(xVars, collapse = '+ ')))
+  } else {
+    modelForm <- as.formula(paste(targetVar, "~", paste(xVars, collapse = '+ '), -1))
+  }
+  return(modelForm)
+}
+
+
+xFeat <- names(train)[c(-1, -9)]
+yFeat <- "interest_level"
+
+modelForm <- createModelFormula(yFeat, xFeat[-31])
+
+
+
+require(doMC)
+require(caret)
+registerDoMC(cores=6)
+library(randomForest)
+
+
+train$distance_city<- log(train$distance_city) 
+# modelForm1 <- createModelFormula(targetVar, c(catVars, numVarsfit))
+
+rf_ctrl<- trainControl(method = "adaptive_boot", 
+                       number =6,
+                       repeats=3,
+                       returnResamp="all",
+                       summaryFunction=mnLogLoss,
+                       verboseIter = TRUE,
+                       classProbs=TRUE,
+                       search = "grid",
+                       allowParallel=TRUE)
+ 
+ rf_fit<- train(modelForm, data = train,
+               method="rf",
+               tuneGrid=expand.grid(mtry=c(6.4)*ncol(train)),
+               metric="mnLogLoss",
+               trControl = rf_ctrl,
+               importance=TRUE,
+               ntree=500 )
+ 
+ 
+control <- trainControl(method="adaptive_cv", number=8, repeats=5, search="grid", verboseIter = TRUE,allowParallel=TRUE , returnResamp="all")
+fit1 <-  randomForest(modelForm, data = train, importance = TRUE, 
+                      ntree = 1000, allowParallel = TRUE, trcontrol = rf_ctrl, verbose=TRUE)
+
+ktest<- read.csv("../processed_data/test_moreFeat36.csv", header = TRUE)
+ktest$distance_city <- log(ktest$distance_city) 
+allpredictions      <- cbind (predsFit1, ktest$listing_id)
+colnames(allpredictions)<-c("high","low","medium","listing_id")
+allpredictions=allpredictions[,c(1,3,2,4)]
+write.csv(allpredictions,paste0(Sys.Date(),"-rpawar2RF_FINXAAAA",seed,".csv"),row.names = FALSE)
+
