124ea682586d6849802a199e0ac9b15f17b77f0e
rpwr021 <rpawar.021@gmail.com>
2017-04-26 21:08:10 -0500

    moved plots

--- Models/RF_forKaggle.R
+++ Models/RF_forKaggle.R
@@ -9,6 +9,10 @@ setwd("/home/raz/Projects/TEMP/EDA_features/Models")
 # train_base <- read.csv('../processed_data/train_baseline11_v2.csv', header = TRUE)
 # train_base1  <- read.csv('../processed_data/train_baselineNEW.csv', header = TRUE)
 train <- read.csv('../processed_data/train_moreFeat36.csv', header = TRUE)
+#sentim <-  read.csv('../processed_data/train_description_sentiment.csv', header = TRUE)
+#View(sentim)
+#train <-merge(train,sentim , by = "listing_id")
+View(train)
 
 ########################
 createModelFormula <- function(targetVar, xVars, includeIntercept = TRUE){
@@ -37,17 +41,59 @@ library(randomForest)
 train$distance_city<- log(train$distance_city) 
 # modelForm1 <- createModelFormula(targetVar, c(catVars, numVarsfit))
 
-control <- trainControl(method="adaptive_cv", number=8, repeats=5, search="grid", verboseIter = TRUE,allowParallel=TRUE , returnResamp="all")
+featExtract <- function(c, lvl) {
+  ###lvl controls what number of count to be considered as factors for numerics
+  if (missing(lvl)) {
+    lvl <- 4
+  }
+  fln <- unique(c)
+  # if (is.numeric(c)) {
+  ifelse(
+    length(fln) == 2 & sum(unique(as.numeric(c))) == 1 ,
+    "bin" ,
+    ifelse(length(fln) >= 3 &
+             length(unique(fln)) < lvl , "factor" , "num")
+  )
+}
+
+
+binFeat <-
+  names(which(lapply(train, featExtract)  == "bin"))
+numFeat <-
+  names(which(lapply(train, featExtract)  == "num"))
+catFeat <-
+  names(which(lapply(train, featExtract)  == "factor"))
+strFeat <-
+  names(which(lapply(train, featExtract)  == "char"))
+
+catFeat<- c(catFeat, "fee", "fitness", "laundry")
+
+train[numFeat] <- lapply(train[numFeat], as.numeric)
+train[catFeat] <- lapply(train[catFeat], as.factor)
+train[binFeat] <- lapply(train[binFeat], as.factor)
+
+train <- droplevels(train)
+
+View(train)
+train$interest_level<-as.integer(factor(train$interest_level))
+
+control <- trainControl(method="adaptive_cv",
+                        number=8,
+                        repeats=5,
+                        search="grid",
+                        verboseIter = TRUE,allowParallel=TRUE , returnResamp="all")
 fit1 <-  randomForest(modelForm, data = train, importance = TRUE, 
-                      ntree = 1000, allowParallel = TRUE, trcontrol = rf_ctrl, verbose=TRUE)
+                      ntree = 600, allowParallel = TRUE, trcontrol = control, verbose=TRUE)
 
 
 gc()
-ktest<- read.csv("../processed_data/test_moreFeat36.csv", header = TRUE)
+  ktest<- read.csv("../processed_data/test_moreFeat36.csv", header = TRUE)
 ktest$distance_city <- log(ktest$distance_city) 
 allpredictions      <- cbind (predsFit1, ktest$listing_id)
 colnames(allpredictions)<-c("high","low","medium","listing_id")
 allpredictions=allpredictions[,c(1,3,2,4)]
+
+
 write.csv(allpredictions,paste0(Sys.Date(),"-rpawar2RF_FINXAAAA",seed,".csv"),row.names = FALSE)
 
 var_importance <- data_frame(variable=setdiff(colnames(train[-1]), "interest_level"),
@@ -65,4 +111,7 @@ p + theme(axis.text.x=element_blank(),
           plot.title=element_text(size=18),
           legend.title=element_text(size=16),
           legend.text=element_text(size=12))
+
+tsne = Rtsne(as.matrix(train), check_duplicates=FALSE, pca=TRUE, 
+             perplexity=30, theta=0.5, dims=2)
 
